d811e7d815437bc57ffa45f9b8ce9d76e09a5819
barnachum203 <barnachum203@gmail.com>
2021-11-01 18:03:09 +0200

    updated seed functions added users seed

--- server/startup/seed.ts
+++ server/startup/seed.ts
@@ -1,39 +1,58 @@
-// require the necessary libraries
 const faker = require("faker");
-const MongoClient = require("mongodb").MongoClient;
+const Task = require("../models/task.model");
+const User = require("../models/user.model");
+const mongoose = require("mongoose");
 
-module.exports = async function seedDB() {
-  // Connection URL
-  const uri = "mongodb://localhost/just_a_like";
-  const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  try {
-    await client.connect();
-    console.log("Connected correctly to server");
-    const collection = client.db("just_a_like").collection("Task");
-    // The drop() command destroys all data from a collection.
-    // Make sure you run it against proper database and collection.
-    collection.drop();
-    // make a bunch of time series data
-    let tasks = [];
-    for (let i = 0; i < 15; i++) {
-      const newTask = {
-        name: faker.name.firstName(),
-        description: faker.lorem.words(),
-        status: faker.datatype.number(100),
-        users: [faker.datatype.number().toString(), faker.datatype.number().toString(),faker.datatype.number().toString()],
-        startDate: Date.now(),
-        finishDate: Date.now(),
-      };
-
-      tasks.push(newTask);
-    }
-    collection.insertMany(tasks);
-    console.log("Database seeded with tasks! :)");
-    // client.close();
-  } catch (err) {
-    console.log(err);
+async function seedTasks() {
+  const res = await Task.deleteMany();
+  if (res) {
+    console.log("Tasks Deleted Successfully");
   }
-};
+  let tasks = [];
+
+  for (let i = 0; i < 15; i++) {
+    const newTask = {
+      name: faker.name.firstName(),
+      description: faker.lorem.words(),
+      status: faker.datatype.number(100),
+      users: [
+        mongoose.Types.ObjectId(),
+        mongoose.Types.ObjectId(),
+        mongoose.Types.ObjectId(),
+      ],
+      startDate: Date.now(),
+      finishDate: Date.now(),
+    };
+
+    tasks.push(newTask);
+  }
+  // tasks:Task
+  await Task.create(tasks);
+  console.log("Tasks Created");
+}
+
+async function seedUsers() {
+  const res = await User.deleteMany();
+  if (res) {
+    console.log("Users Deleted Successfully");
+  }
+  let users: any = [];
+
+  for (let i = 0; i < 15; i++) {
+    const newUser = {
+      first_name: faker.name.firstName(),
+      last_name: faker.name.lastName(),
+      email: faker.internet.email(),
+      phone_number: faker.datatype.number(900000),
+      linkedin_url: faker.internet.email(),
+      experties: ["617fcc43e6aad249c573c757"],
+      interests: ["617fcc43e6aad249c573c758"],
+      level: 4,
+    };
+
+    users.push(newUser);
+  }
+  await User.create(users);
+  console.log("Users Created");
+}
+module.exports = { seedTasks, seedUsers };
