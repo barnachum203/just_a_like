5b3fd16faf8c651b54d645aacbe862e37f09220a
barnachum203 <barnachum203@gmail.com>
2021-11-01 18:22:52 +0200

    return to v1

--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,33 @@
-import express, { Application } from "express";
+import express, { Application } from 'express';
+const mongoose = require("mongoose");
+const cors = require('cors');
 const app: Application = express();
 
-app.use(require('body-parser').json())
-require("dotenv").config();
-// require("./startup/db")();
-require("./startup/routes")(app);
-// require("./startup/seed")();
+app.use(cors({ 
+  credentials: true
+})); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const port = process.env.PORT || 8000;
+require("dotenv").config();
+require("./startup/db")();
+
+const seeds = require("./startup/seed");
+
+//Uncomment to seed db
+// seeds.seedTasks();
+// seeds.seedUsers();
+
+
+// const projectRoutes = require('./routes/project');
+// const userRoutes = require('./routes/user');
+const tasksRoutes = require('./routes/task');
+
+// app.use('/project', projectRoutes);
+// app.use('/user', userRoutes);
+app.use('/task', tasksRoutes);
+    
+const port = process.env.PORT|| 8000;
 app.listen(port, () => {
   console.log("Connect succesfully on port: " + port);
 });
